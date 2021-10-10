d870000b4527de607d494a7c1f3e4793b35b0b88
Kerwin Lonono <klondono1016@gmail.com>
2021-10-10 19:16:43 -0400

    google auth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import auth from 'vue3-google-oauth2';
+
+const app = createApp(App);
+
+let gauthClientId = "983832817396-tumit47sfk7frtcn7o916bov1qovrb82.apps.googleusercontent.com";
+
+app.use(auth, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: false });
+
+app.mount('#app');
